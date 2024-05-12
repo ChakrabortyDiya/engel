9c3ce2de3cdd7fe1dff563793abf493b96dd6334
ChakrabortyDiya <crs.diya@gmail.com>
2024-05-12 17:22:13 +0530

    updated

--- engel.py
+++ engel.py
@@ -16,3 +16,6 @@ def speak(audio):
 def take_command():
     r = sr.Recognizer()
     with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
