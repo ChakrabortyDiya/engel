329668ef4f36eb59887cca67a3db913e2f88e647
ChakrabortyDiya <crs.diya@gmail.com>
2024-05-12 17:33:37 +0530

    updated

--- engel.py
+++ engel.py
@@ -41,4 +41,5 @@ while True:
             speak("Searching Wikipedia ...")
             query = query.replace("wikipedia", '')
             results = wikipedia.summary(query, sentences=2)
-            
+            speak("According to wikipedia")
+            speak(results)
