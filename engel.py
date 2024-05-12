d8382a72b19d9f762860fb8647103ff8b4c98d9a
ChakrabortyDiya <crs.diya@gmail.com>
2024-05-12 17:19:58 +0530

    updated

--- engel.py
+++ engel.py
@@ -8,3 +8,7 @@ engine = pyttsx3.init("sapi5")
 voices = engine.getProperty("voices")
 
 engine.setProperty('voice', voices[1].id)  
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
